380ed4b581fa6cbcc3afac4bd32a09433801c4a6
Abuzar84 <sayyedabuzar021@gmail.com>
2025-09-04 23:52:15 +0530

    payment input area added

--- js/basic.js
+++ js/basic.js
@@ -2,8 +2,10 @@
 const container = document.getElementById("container");
 const paymentButton = document.getElementById("payment");
 const buttonsContainer = document.getElementById("buttons");
-
-
+const span_links = document.getElementById('links');
+const dateSpan = document.getElementById('date');
+const date = new Date();
+const year = date.getFullYear();
 
 const createButtons = document.createElement('div');
 buttonsContainer.appendChild(createButtons);
@@ -14,6 +16,17 @@ const addButtons = [
     {name:"Payment",url:"payment.html"},
     {name:"Receipt",url:"receipt.html"},
     {name:"Cash Balance",url:"cashbalance.html"},
-    {name:"Bank Balance",url:"bankbalance.html"},
+    {name:"Bank Balance",url:"bankbalance.html"}
 ];
 createButtons.innerHTML = addButtons.map(but =>`<div class="accountingButtons"><a href="${but.url}">${but.name}</a></div>`).join("");
+//add quick links in footer.
+const addLinks = [
+    {linksname: 'Payment', linksurl:'payment.html'},
+    {linksname: 'Receipt', linksurl:'receipt.html'},
+    {linksname:"Cash Balance",linksurl:"cashbalance.html"},
+    {linksname:"Bank Balance",linksurl:"bankbalance.html"}
+];
+
+span_links.innerHTML = addLinks.map(links => `<p><a href='${links.linksurl}'>${links.linksname}`).join("");
+
+dateSpan.innerHTML = year;
